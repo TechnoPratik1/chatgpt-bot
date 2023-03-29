8e3f1912f5a140cb6dc0e1cb622e2d91080f35aa
TechnoPratik1 <129277007+TechnoPratik1@users.noreply.github.com>
2023-03-29 22:58:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv("config.env")
 
 API_ID = int(getenv("API_ID", "25843661"))
 API_HASH = getenv("API_HASH", "5e5bdce7ff26e46d224d62298850dec8")
-BOT_TOKEN = getenv("BOT_TOKEN", "6107856966:AAEFb3iNHHzpuEeneQhi6NsEugLlcjCequ4")
-OPENAI_API = getenv("OPENAI_API", "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") # get api key : https://platform.openai.com/account/api-keys
+BOT_TOKEN = getenv("BOT_TOKEN", "6200553989:AAE5ROwTGD0i91beW7Sw9iL5HVgyeyQ2WHA")
+OPENAI_API = getenv("OPENAI_API", "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") # get api key : https://platform.openai.com/account/api-keys
